a056e041c9b7eaee6c32bd60761cc06fafbf2fd1
tipka <tipkanet@gmail.com>
2021-11-24 19:35:36 +0200

    add single user model

---
+++ src/test/java/models/SingleUserData.java
@@ -0,0 +1,13 @@
+package models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+
+public class SingleUserData {
+    @JsonProperty("data")
+    private SingleUser singleUser;
+}
